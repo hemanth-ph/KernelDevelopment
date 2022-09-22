0e8b936bd929da418d40bfe1c51a781233d95996
Hemanth Pippalla <pippallahemanth99@gmail.com>
2022-09-23 00:37:23 +0530

    Bootloader's Address is specified through DS register

--- 002_print_string/bootloader.asm
+++ 002_print_string/bootloader.asm
@@ -1,13 +1,15 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 start:
-    mov si, message
+    mov ax,0X7C0 ;we cannot directly load into seg regs. so we are using ax as via.
+    mov ds,ax    ;instead of specifying at ORG, we are loading 0x7c0(seg. addr.) to DS
+    mov si, message ; We are loading offset of msg to SI
     call print
     jmp $
 print:
     mov bx,0
 .loop:
-    lodsb
+    lodsb ;lodsb will use DS:SI combination
     cmp al, 0
     je .done
     call print_char
