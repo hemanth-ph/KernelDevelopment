6c2894f73bfb49a79464fe79079e2821209546f3
Hemanth Pippalla <pippallahemanth99@gmail.com>
2022-09-30 00:25:46 +0530

    implemented interrupt service routine and changed divize by zero's IR handler to our interrupt handler

--- 002_print_string/bootloader.asm
+++ 002_print_string/bootloader.asm
@@ -8,15 +8,28 @@ times 33 db 5
 
 start:
     jmp 0x7c0:start2
+
+handle_zero: ;our interrupt handler 
+    mov ah,0eh
+    mov al,'A'
+    mov bx,0x00
+    int 0x10
+    iret
+
 start2:
     cli ;clear all the interrupts
     mov ax,0X7C0 ;we cannot directly load into seg regs. so we are using ax as via.
     mov ds,ax    ;instead of specifying at ORG, we are loading 0x7c0(seg. addr.) to DS
-    mov si, message ; We are loading offset of msg to SI
-    mov ax,0
-    mov ss,ax  ;stack segment
+    mov es,ax 
+    mov ax,0x00
     mov sp,0x7c00
     sti ;enable all the interrupts 
+
+    mov word[ss:0x00], handle_zero ;first two bytes pointing to offset of interrupt handler
+    mov word[ss:0x02], 0x7c0 ;next two bytes to segment address
+    int 0 ;Triggering interrupt
+    
+    mov si, message ; We are loading offset of msg to SI
     call print
     jmp $
 print:
@@ -32,7 +45,7 @@ print:
 
 print_char:
     mov ah, 0eh
-    int 0x10
+    int 0x10 ;interrupt for print character on screen
     ret
 message:
     db 'Hi hemanth!!', 0
